395d476d6079f2011e005e33bfc4bd9363ddfab1
KonBas <baskov_konstantin@mail.ru>
2021-09-28 14:28:38 +0300

    Fix Canvas X just Int

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -19,8 +19,27 @@
     </div>
 
     <div class="image">
-        <canvas class="canvas" width="500" height="500" id="canvas" onmousedown = "  ">
+        <canvas class="canvas" width="500" height="500" id="canvas"
+                onmousemove = "MouseXY.value = event.clientX+':'+event.clientY "
+                onclick = " let Ypass = false;
+                            let Xpass = false;
+                            if ((parseInt((event.clientX - 720)/40) <= 4) && (parseInt((event.clientX - 720)/40) >= -4)){
+                                X.value = parseInt((event.clientX - 720)/40);
+                                Xpass = true;
+                            } else alert('X вышел за пределы допустимых значений');
+                            if (((505 - event.clientY )/40 >= -5) && ((505 - event.clientY )/40 <= 3)){
+                                cordY.value = (505 - event.clientY )/40;
+                                Ypass = true;
+                            } else alert('Y вышел за пределы допустимых значений');
+
+                            if (Xpass && Ypass){
+                            if (R.value>0){
+                                submint.click();
+                            }
+                            else alert('Вы не выбрали R');
+                                }">
         </canvas>
+
     </div>
 
     <div class="inputvalue">
@@ -41,7 +60,7 @@
                 <span class = "checkboxV"> <input type="checkbox" name="coordinate_X" value="2" onclick=" X.value = 2 ">2</span>
                 <span class = "checkboxV"><input type="checkbox" name="coordinate_X" value="3" onclick=" X.value = 3 ">3</span>
                 <span class = "checkboxV"><input type="checkbox" name="coordinate_X" value="4" onclick=" X.value = 4 ">4</span>
-                    <input type="hidden" id="X" name="coor_X">
+                    <input type="text" id="X" name="coor_X">
 
                 <div class="tablevalue">
                     <div>
@@ -63,6 +82,7 @@
                 <div class="buttonsent"><button id="submint" type="submit">Отправить</button></div>
                 <div class="buttonresent"><button id="resent" type="reset">Сбросить</button></div>
             </div>
+            <p>X:Y ==<input id="MouseXY" name = "MouseXY" type="text"></p>
         </form>
     </div>
     <div class="outputvalue">
--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,86 +1,142 @@
-$(function (){
-        canvas = document.getElementById("canvas"); // строка сценария извлекает узел в модели DOM, представляющий <canvas>элемент, путем вызова document.getElementById()метода
-        var ctx = canvas.getContext("2d"); // Когда у нас есть узел элемента, вы можете получить доступ к контексту рисования, используя его getContext()метод.
-        var r = 2;
-        setInterval( draw ,20);
+$(function () {
+    canvas = document.getElementById("canvas"); // строка сценария извлекает узел в модели DOM, представляющий <canvas>элемент, путем вызова document.getElementById()метода
+    var ctx = canvas.getContext("2d"); // Когда у нас есть узел элемента, вы можете получить доступ к контексту рисования, используя его getContext()метод.
+    setInterval(draw, 20);
 
-        function windowToCanvas(canvas, x, y) {
-            let bbox = canvas.getBoundingClientRect();
-            return { x: x - bbox.left * (canvas.width / bbox.width),
-                y: y - bbox.top * (canvas.height / bbox.height)
-            };
+    function draw() {
+        let R = parseInt(document.getElementById("R").value);
+        let step = 20;
+
+        ctx.clearRect(0, 0, 500, 500);
+        ctx.beginPath();
+
+        ctx.font = "8px Georgia";
+        ctx.fillStyle = "#000000";
+
+        drawAxis();
+        drawMarkUp(step, 5);
+        drawFigure(R, step);
+
+        ctx.closePath();
+
+        drawTable();
+
+
+
+    }
+
+    function drawTable(){
+        let cells = Array.prototype.slice.call(document.getElementById("table_data").getElementsByTagName("td"));
+        let n = cells.length;
+        if(Number(cells[2].innerHTML) !== 0){
+            for(let i = 0; i < n; i = i + 6){
+                drawPoint(Number(cells[i].innerHTML), Number(cells[i + 1].innerHTML),cells[i + 5].innerHTML);
+            }
         }
 
-        function drawPoint(x, y, r, result) {
-            ctx.beginPath();
-            r=1;
-            if (result == "Балдеж") {
-                ctx.fillStyle = "rgba(0, 255, 0, 1)";
-            }else {
-                ctx.fillStyle = "rgba(255, 0, 0, 1)";
-            }
-            ctx.arc(250 + 2*x*20, 250 - 2*y*20, 3, 0, 2 * Math.PI);
-            ctx.closePath();
-            ctx.fill();
+    }
+
+    function drawPoint(x,y,Result)
+    {
+        ctx.beginPath()
+        if (Result == "Балдеж") ctx.fillStyle = "rgb(0,255,0)";
+        else ctx.fillStyle = "rgb(255,0,0)";
+        ctx.arc(250 + 2*x*20, 250 - 2*y*20, 3, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+
+        function drawFigure(R,step)
+        {
+            ctx.moveTo(250,250 - step*R);
+            ctx.lineTo(250 - step*R,250);
+            ctx.strokeStyle = '#000000';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
+            ctx.moveTo(250-2*step*R,250);
+            ctx.lineTo(250-2*step*R,250 + step*R);
+            ctx.strokeStyle = '#000000';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
+            ctx.moveTo(250-2*step*R,250 + step*R);
+            ctx.lineTo(250,250 + step*R);
+            ctx.strokeStyle = '#000000';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
+            ctx.arc(250, 250, 2*step * R, -Math.PI / 2, 0, false);
+            ctx.strokeStyle = '#000000';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
 
         }
 
-        function checkTableAndDraw(){
-            let cells = Array.prototype.slice.call(document.getElementById("table_data").getElementsByTagName("td"));
-            let n = cells.length;
-            if(Number(cells[2].innerHTML) !== 0){
-                for(let i = 0; i < n; i = i + 6){
-                    drawPoint(Number(cells[i].innerHTML), Number(cells[i + 1].innerHTML), Number(cells[i + 2]).innerHTML, "Есть пробитие");
-                }
+        function drawMarkUp(step,MarkStep)
+            {
+                ctx.moveTo(250+step*5,255);
+                ctx.lineTo(250+step*5,245);
+                ctx.strokeStyle = '#000000';
+                ctx.lineWidth = 2;
+                ctx.stroke();
+                ctx.fillText(String(MarkStep/2), 250 + step*5, 265);
+
+                ctx.moveTo(250+2*step*5,253);
+                ctx.lineTo(250+2*step*5,247);
+                ctx.strokeStyle = '#000000';
+                ctx.lineWidth = 2;
+                ctx.stroke();
+                ctx.fillText(""+String(MarkStep), 467, 262);
+
+                ctx.moveTo(250-step*5,253);
+                ctx.lineTo(250-step*5,247);
+                ctx.strokeStyle = '#000000';
+                ctx.lineWidth = 2;
+                ctx.stroke();
+                ctx.fillText("-"+String(MarkStep/2), 135, 262);
+
+                ctx.moveTo(250-2*step*5,253);
+                ctx.lineTo(250-2*step*5,247);
+                ctx.strokeStyle = '#000000';
+                ctx.lineWidth = 2;
+                ctx.stroke();
+                ctx.fillText("-"+String(MarkStep), 30, 262);
+
+                ctx.moveTo(253,250 + step*5);
+                ctx.lineTo(247,250 + step*5);
+                ctx.strokeStyle = '#000000';
+                ctx.lineWidth = 2;
+                ctx.stroke();
+                ctx.fillText("-"+String(MarkStep/2), 262, 363);
+
+                ctx.moveTo(253,250 + 2*step*5);
+                ctx.lineTo(247,250 + 2*step*5);
+                ctx.strokeStyle = '#000000';
+                ctx.lineWidth = 2;
+                ctx.stroke();
+                ctx.fillText("-"+String(MarkStep), 262, 473);
+
+                ctx.moveTo(253,250 - step*5);
+                ctx.lineTo(247,250 - step*5);
+                ctx.strokeStyle = '#000000';
+                ctx.lineWidth = 2;
+                ctx.stroke();
+                ctx.fillText(String(MarkStep/2), 262, 144);
+
+                ctx.moveTo(253,250 - step*5);
+                ctx.lineTo(247,250 - step*5);
+                ctx.strokeStyle = '#000000';
+                ctx.lineWidth = 2;
+                ctx.stroke();
+                ctx.fillText(String(MarkStep), 262, 34);
+
             }
-        }
 
-        canvas.onmousedown = function (e) {
-            let s = 0;
-            let radR = document.getElementById("R");
-            if ((radR.value !=1)&&(radR.value !=2)&&(radR.value !=3)&&(radR.value !=4)&&(radR.value !=5)) {
-                alert("Вы не выбрали R!");
-                return 0;
-            }else if(radR.value == 1) {
-                s = 1;
-            }
-            else if (radR.value == 2) {
-                s = 2;
-            }else if(radR.value == 3) {
-                s = 3;
-            }else if(radR.value == 4) {
-                s = 4;
-            }else if (radR.value == 5) {
-                s = 5;
-            }
+        function drawAxis(){
 
-
-            let corArr = windowToCanvas(canvas, e.clientX, e.clientY);
-            let x_real =  ((corArr.x - 250)/175);
-            let y_real = (-1) *((corArr.y - 250)/175);
-
-            //drawPoint(x_real,y_real,radR.value,"Есть пробитие");
-
-            let out = document.getElementById("X");
-            out.value = parseInt(x_real*s);
-
-            out = document.getElementById("cordY");
-            out.value = (y_real*s).toFixed(3);
-
-            /* пока что коменчу что бы отладить правильный тык */
-            out = document.getElementById("submint");
-            out.click();
-        }
-
-        function draw(){
-
-            let R = parseInt(document.getElementById("R").value);
-            let step = 20;
-
-            ctx.clearRect(0,0, 500, 500);
-            ctx.beginPath();
-
-            ctx.fillStyle = "rgba(255, 0, 0, 1)";
 
             ctx.moveTo(250,490);
             ctx.lineTo(250,10);
@@ -105,7 +161,7 @@ $(function (){
             ctx.strokeStyle = '#000000';
             ctx.lineWidth = 2;
             ctx.stroke();
-            ctx.fillText("Y", 245, 10);
+            ctx.fillText("Y", 235, 10);
 
             ctx.moveTo(490,250);
             ctx.lineTo(483,257);
@@ -118,88 +174,9 @@ $(function (){
             ctx.strokeStyle = '#000000';
             ctx.lineWidth = 2;
             ctx.stroke();
-            ctx.fillText("X", 490, 255);
-
-            ctx.moveTo(250+step*5,255);
-            ctx.lineTo(250+step*5,245);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.fillText("2.5", 250 + step*5, 265);
-
-            ctx.moveTo(250+2*step*5,253);
-            ctx.lineTo(250+2*step*5,247);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.fillText("5", 467, 262);
-
-            ctx.moveTo(250-step*5,253);
-            ctx.lineTo(250-step*5,247);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.fillText("-2.5", 135, 262);
-
-            ctx.moveTo(250-2*step*5,253);
-            ctx.lineTo(250-2*step*5,247);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.fillText("-5", 30, 262);
-
-            ctx.moveTo(253,250 + step*5);
-            ctx.lineTo(247,250 + step*5);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.fillText("-2.5", 262, 363);
-
-            ctx.moveTo(253,250 + 2*step*5);
-            ctx.lineTo(247,250 + 2*step*5);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.fillText("-5", 262, 473);
-
-            ctx.moveTo(253,250 - step*5);
-            ctx.lineTo(247,250 - step*5);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.fillText("2.5", 262, 144);
-
-            ctx.moveTo(253,250 - step*5);
-            ctx.lineTo(247,250 - step*5);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.fillText("5", 262, 34);
-
-            ctx.moveTo(250,250 - step*R);
-            ctx.lineTo(250 - step*R,250);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-
-            ctx.moveTo(250-2*step*R,250);
-            ctx.lineTo(250-2*step*R,250 + step*R);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-
-            ctx.moveTo(250-2*step*R,250 + step*R);
-            ctx.lineTo(250,250 + step*R);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-
-            ctx.arc(250, 250, 2*step * R, -Math.PI / 2, 0, false);
-            ctx.strokeStyle = '#000000';
-            ctx.lineWidth = 2;
-            ctx.stroke();
-
-            checkTableAndDraw();
+            ctx.fillText("X", 490, 265);
         }
+
+
     }
 );
